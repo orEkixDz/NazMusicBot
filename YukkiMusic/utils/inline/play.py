58ac7c3bd5e6a38cf25977781dc25c2c62651ad6
iLOSTinYOU <87181511+orekixadi@users.noreply.github.com>
2022-11-12 10:47:55 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -77,7 +77,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         [
             InlineKeyboardButton(
                        text="📡Join",
-                       url='https://t.me/Loveisaslowpoision'), 
+                       url='https://t.me/Loveisaslowpoison'), 
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             )
@@ -140,7 +140,7 @@ def stream_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                        text="📡Join",
-                       url='https://t.me/loveisaslowpoision'), 
+                       url='https://t.me/loveisaslowpoison'), 
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             )
