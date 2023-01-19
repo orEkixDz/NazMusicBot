542f49fe283467955b428afdd718967da75be4ed
orEki <122883292+orEkixDz@users.noreply.github.com>
2023-01-19 16:54:44 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -77,7 +77,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         [
             InlineKeyboardButton(
                        text="📡Join",
-                       url='https://t.me/Loveisaslowpoison'), 
+                       url='https://t.me/+osYLwtx3p-swN2Q1'), 
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             )
@@ -140,7 +140,7 @@ def stream_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                        text="📡Join",
-                       url='https://t.me/loveisaslowpoison'), 
+                       url='https://t.me/+osYLwtx3p-swN2Q1'), 
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             )
